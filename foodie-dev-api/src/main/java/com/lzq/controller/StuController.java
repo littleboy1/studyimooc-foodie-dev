c0b6af047246d7be91e82243ca158c589e60a863
812551543@qq.com <812551543@qq.com>
2019-11-19 23:33:06 +0800

    编写增删改查通用接口

--- foodie-dev-api/src/main/java/com/lzq/controller/StuController.java
+++ foodie-dev-api/src/main/java/com/lzq/controller/StuController.java
@@ -3,6 +3,7 @@ package com.lzq.controller;
 import com.lzq.service.StuService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -17,4 +18,21 @@ public class StuController {
     public Object getStu(int id){
         return stuService.getStuInfo(id);
     }
+
+    @PostMapping("/saveStu")
+    public Object saveStu(){
+         stuService.saveStu();
+         return "200";
+    }
+    @PostMapping("/updateStu")
+    public Object updateStu(int id){
+        stuService.updateStu(id);
+        return "200";
+    }
+    @PostMapping("/deleteStu")
+    public Object deleteStu(int id){
+        stuService.deleteStu(id);
+        return "200";
+    }
+
 }
--- foodie-dev-service/src/main/java/com/lzq/service/impl/StuServiceImpl.java
+++ foodie-dev-service/src/main/java/com/lzq/service/impl/StuServiceImpl.java
@@ -18,19 +18,26 @@ public class StuServiceImpl implements StuService {
     public Stu getStuInfo(int id) {
         return stuMapper.selectByPrimaryKey(id);
     }
-
+    @Transactional(propagation = Propagation.REQUIRED)
     @Override
     public void saveStu() {
-
+        Stu stu = new Stu();
+        stu.setName("jack");
+        stu.setAge(19);
+        stuMapper.insert(stu);
     }
-
+    @Transactional(propagation = Propagation.REQUIRED)
     @Override
     public void updateStu(int id) {
-
+        Stu stu = new Stu();
+        stu.setName("tom");
+        stu.setAge(500);
+        stu.setId(id);
+        stuMapper.updateByPrimaryKey(stu);
     }
-
+    @Transactional(propagation = Propagation.REQUIRED)
     @Override
     public void deleteStu(int id) {
-
+        stuMapper.deleteByPrimaryKey(id);
     }
 }
